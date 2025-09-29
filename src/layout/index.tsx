5487195b3ab54ab95de64e3540e290a38601aa31
Royalaid <2439803+royalaid@users.noreply.github.com>
2025-09-29 07:57:18 -0700

    remove unneeded footer

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -23,7 +23,6 @@ const Layout: React.FC<Props> = ({ children }) => {
                 <div className="wrapper">{children}</div>
             </main>
 
-            <Footer />
             <Toaster />
         </main>
     );
